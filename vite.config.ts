c758d032ab4696e0505b91a861a602001bf8f7c7
Emanuel Lorenzo <aemalorenzo@gmail.com>
2024-08-31 22:27:19 -0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,30 @@
-// import build from '@hono/vite-cloudflare-pages'
+import build from '@hono/vite-cloudflare-pages'
 import devServer from '@hono/vite-dev-server'
 import adapter from '@hono/vite-dev-server/cloudflare'
 import { defineConfig } from 'vite'
-import { resolve } from "node:path";
-import cloudflare from '@hono/vite-cloudflare-pages'
 
-export default defineConfig({
-  ssr: {
-    external: ['react', 'react-dom']
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        // main: resolve(__dirname, "src/index.tsx")
-        server: 'src/index.tsx',
-        client: 'src/client.tsx'
+export default defineConfig(({ mode }) => {
+  if (mode === 'client') {
+    return {
+      esbuild: {
+        jsxImportSource: 'hono/jsx/dom'
+      },
+      build: {
+        manifest: true,
+        rollupOptions: {
+          input: './src/client.tsx'
+        }
       }
     }
-  },
-  plugins: [
-    // build(),
-    cloudflare(),
-    devServer({
-      adapter,
-      entry: 'src/index.tsx'
-    })
-  ]
+  } else {
+    return {
+      plugins: [
+        build(),
+        devServer({
+          adapter,
+          entry: 'src/index.tsx'
+        })
+      ]
+    }
+  }
 })
